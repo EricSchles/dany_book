d99a05a1d7480472cb613820da93a0b94abbca1e
EricSchles <ericschles@gmail.com>
2015-06-17 09:53:09 -0700

    things

--- basic/finding_names.py
+++ basic/finding_names.py
@@ -1,3 +1,8 @@
 f = open("file.txt","r")
 contents = f.read().split("\n")
 counts = {"Eric":0,"Bryan":0,"Connor":0}
+for line in contents:
+    for name in counts:
+        if name in line:
+            counts[name] += 1
+print counts
