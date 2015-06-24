bcbb8194ef3a9d07e72889a3ba18fdd7820e31f7
EricSchles <ericschles@gmail.com>
2015-06-24 07:21:02 -0700

    trees

--- basic/tree.py
+++ basic/tree.py
@@ -6,31 +6,46 @@ class Node:
     def __str__(self):
         return repr(self.data)
 
+    def has_next(self):
+        if self.left != None or self.right != None:
+            return True
+        else:
+            return False
+        
 class Tree:
     def __init__(self,data=None):
         self.head = Node(data)
+
     def append(self,data):
         if not self.head:
             self.head = Node(data)
         else:
-            cur = self.head
-            while cur.next:
-                cur = cur.next
-            if data <= cur.data:
-                cur.left = Node(data)
-            elif data > cur.next:
-                cur.right = Node(data)
+            self._append(self.head,data)
+
+    def _append(self,cur,data):
+        if cur.data > data and cur.left == None:
+            cur.left = Node(data)
+        elif cur.data < data and cur.right == None:
+            cur.right = Node(data)
+        else:
+            if cur.data < data:
+                self._append(cur.right,data)
+            else:
+                self._append(cur.left,data)
+                        
     def pretty_print(self):
         cur = self.head
-        self.pprint(self,cur)
+        self.pprint(cur)
 
     def pprint(self,cur):
-        pprint(cur.left)
+        if cur.left:
+            self.pprint(cur.left)
         print cur
-        pprint(cur.right)
+        if cur.right:
+            self.pprint(cur.right)
 
 if __name__ == '__main__':
     tree = Tree(1)
-    for i in xrange(100000):
+    for i in xrange(100):
         tree.append(i)
     tree.pretty_print()	
